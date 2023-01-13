c0995cc544abf80f38293594646676b92b2bee7d
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2023-01-13 12:54:30 +0500

    13-01-2023

---
+++ 13-01-2023/app.js
@@ -0,0 +1,63 @@
+// let start=+prompt("Enter any start year")
+// let end=+prompt("Enter an end year")
+
+// for(let i=start;i<=end;i++){
+//     if(i%4===0){
+//         document.write(i+"<br>")
+//     }
+// }
+
+// let data = ["cake", "apple pie", "cookie", "chips", "patties"]
+// let item=prompt("enter what you want")
+
+// for(let i=0;i<data.length;i++){
+//     if(item.toLowerCase()===data[i]){
+//         alert("it is available")
+//     }
+// }
+
+// let str=prompt("Enter any string")
+// let len=str.charAt(str.length-1)
+// switch(len.toLowerCase()){
+//     case "a":
+//         document.write("It is a vowel")
+//         break
+//     case "e":
+//         document.write("It is a vowel")
+//         break
+//     default:
+//         document.write("It is a consonant")
+//         break
+// }
+
+// let std=["Ali","Rooman","Sadiq"]
+// let marks=[320,230,480]
+// let per=0
+// for(let i=0;i<std.length;i++){
+//     per=(marks[i]/500)*100
+//     document.write("Score of "+std[i] +" is "+marks[i]+"."+" Percentage: "+per+"%"+"<br>")
+// }
+
+// let str=prompt("Enter any string")
+// let len=str.charAt(str.length-1)
+// if(len.toLowerCase()==="a"){
+//     document.write("Vowel")
+// }
+// else if(len.toLowerCase()==="e"){
+//     document.write("Vowel")
+// }
+// else if(len.toLowerCase()==="i"){
+//     document.write("Vowel")
+// }
+// else if(len.toLowerCase()==="o"){
+//     document.write("Vowel")
+// }
+// else if(len.toLowerCase()==="u"){
+//     document.write("Vowel")
+// }
+// else{
+//     document.write("consonant")
+// }
+
+
+
